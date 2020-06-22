4a98960cb15c368750bcbe4fa51356a49b154f9e
winsen <29982851@qq.com>
2020-06-22 09:54:45 +0800

    add gamma screen coord ddx_ddy

---
+++ unity_shader/Assets/Editor/Test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Test
+{
+	[MenuItem("Assets/Custom/PrintMiddlePixel")]
+	static void PrintMiddlePixel()
+	{
+		Texture2D tex = Selection.activeObject as Texture2D;
+		Color c = tex.GetPixel(tex.width / 2, tex.height / 2);
+		Debug.Log(c);
+	}
+}
